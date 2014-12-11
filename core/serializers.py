cd142544791af997727d32d5d5e259f4202418d5
Denis K <f1nal@cgaming.org>
2014-12-11 19:22:19 +0300

    Add password

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from core.models import User
 
@@ -6,4 +7,8 @@ from core.models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'password')
+
+    def restore_object(self, attrs, instance=None):
+        attrs['password'] = make_password(attrs['password'])
+        return super(UserSerializer, self).restore_object(attrs, instance=None)
